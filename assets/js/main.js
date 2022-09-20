7d21ff2ee5cb193a063e5177723cbffd61bf0817
Kate Hynes <kahynes@davidson.edu>
2022-09-20 10:28:20 -0400

    class work 9/19

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
 
 /* javascript */
+// $("h1").hide();
+$(".clickme").click(function(eventObject) {
+  console.log("ah", eventObject);
+});
+
+let username = "";
+
+$("form").submit(function(e) {
+  e.preventDefault();
+  username = $(".username").val();
+  console.log(username);
+});
