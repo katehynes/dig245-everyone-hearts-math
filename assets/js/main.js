62991357b3518385f9c9c2dfa623cec43feba5bb
Kate Hynes <kahynes@davidson.edu>
2022-09-22 16:33:30 -0400

    got console to print correct # of days between

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,45 @@
 /* javascript */
 // $("h1").hide();
-$(".clickme").click(function(eventObject) {
-  console.log("ah", eventObject);
-});
+//$(".clickme").click(function(eventObject) {
+  //console.log("ah", eventObject);
+//}
 
-let username = "";
-let requiredUsername = "secret";
+
+const MILLI_PER_DAY = 1000 * 60 * 60 * 24;
 
 $("form").submit(function(e) {
   e.preventDefault();
-  username = $(".username").val();
-  console.log(username);
+  let firstDay = new Date(document.getElementById("firstDay").value);
+  let secondDay = new Date(document.getElementById("secondDay").value);
+  calculateDays(firstDay, secondDay);
+})
 
-  let str = "";
-
-  if (username == requiredUsername) {
-    str = `
-    <div class="alert alert-success" role="alert">
-    Welcome, friend. </div>
-    `
-  } else {
-    str = `
-    <div class="alert alert-danger" role="alert">Not allowed</div>
-    `
+function calculateDays(firstDay, secondDay) {
+  let distance;
+  let time1 = firstDay.getTime();
+  let time2 = secondDay.getTime();
+  if (time1 < time2) {
+    distance = time2 - time1;
   }
+  else {
+    distance = time1 - time2;
+  }
+  distance = distance / MILLI_PER_DAY;
+  console.log("Number of days between " + firstDay + " and " + secondDay + " is: " + distance);
+}
 
-  $(".output").html(str);
-});
+  //let str = "";
+
+//   if (username == requiredUsername) {
+//     str = `
+//     <div class="alert alert-success" role="alert">
+//     Welcome, friend. </div>
+//     `
+//   } else {
+//     str = `
+//     <div class="alert alert-danger" role="alert">Not allowed</div>
+//     `
+//   }
+//
+//   $(".output").html(str);
+// });
